a16c19e6b27dbab9eb59c98554e1e04a5b1ccbf8
Jonathan Bramble <jonathan.bramble@ubc.ca>
2016-11-10 20:15:14 -0800

    added functions for Ha and Hb

--- main.m
+++ main.m
@@ -23,15 +23,15 @@ H_an(:,1) = D1_nz(nm,z2(1));
 H_bn(:,1) = D1_nz(nm,z2(1));  
 
 for l=2:layers
-    % do I need to keep these? nope
-    D1z2=D1_nz(nm,z2(l));
-    D1z1=D1_nz(nm,z1(l));
-    D3z2=D3_nz(D1z1,z2(l));
-    D3z1=D3_nz(D1z2,z1(l));
-    %G1, G2, Qn
-    G1 = m(l)*H_an(:,(l-1))-m(l-1)*D1z1';
-    G2 = m(l)*H_an(:,(l-1))-m(l-1)*D3z1';
-    Gt1 = m(l-1)*H_bn(:,(l-1))-m(l)*D1z1';
-    Gt2 = m(l-1)*H_bn(:,(l-1))-m(l)*D3z1';
-    Q_nl(nm,x,l,z1(l),z2(l),D1z2,D3z2,D1z1,D3z1);
+    D1z2=D1_nz(nm,z2(l))';
+    D1z1=D1_nz(nm,z1(l))';
+    D3z2=D3_nz(D1z1,z2(l))';
+    D3z1=D3_nz(D1z2,z1(l))';
+    G1 = m(l)*H_an(:,(l-1))-m(l-1)*D1z1;
+    G2 = m(l)*H_an(:,(l-1))-m(l-1)*D3z1;
+    Gt1 = m(l-1)*H_bn(:,(l-1))-m(l)*D1z1;
+    Gt2 = m(l-1)*H_bn(:,(l-1))-m(l)*D3z1;
+    Qn = Q_nl(nm,x,l,z1(l),z2(l),D1z2,D3z2,D1z1,D3z1)';
+    H_an(:,l) = (G2.*D1z2 - Qn.*G1.*D3z2)./(G2-Qn.*G1);
+    H_bn(:,l) = (Gt2.*D1z2 - Qn.*Gt1.*D3z2)./(Gt2-Qn.*Gt1);
 end
