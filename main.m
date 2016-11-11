663fb49d9507beac38787b81d38543cfd71cce52
Jonathan Bramble <jonathan.bramble@ubc.ca>
2016-11-11 15:02:36 -0800

    testing out with ag data

--- main.m
+++ main.m
@@ -1,16 +1,15 @@
 Nm = 1.33;
 
 %lambda = linspace(300,700);
-lambda = gold(:,1)
-r1 = 40;
-r2 = 41;
+r1 = 20;
+r2 = 21;
 
 r = [r1 r2];
 
 for l=1:length(lambda)
-m = [(gold(l,2)+1i*gold(l,3))/Nm 1.45/Nm];
-x = 2*pi*Nm*r/lambda(l);
-[Qext(l), Qsca(l)] = nmie(m,x);
+    m = [(ag_n(l)+1i*ag_k(l))/Nm 1.45/Nm];
+    x = 2*pi*Nm*r/lambda(l);
+    [Qext(l), Qsca(l)] = nmie(m,x);
 end
 
 plot(lambda,Qsca,'r')
